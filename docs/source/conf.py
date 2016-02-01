4dcd4be29b428b87f42827d19bb433a4692b17dd
Antoine Letarte <antoine.letarte@gmail.com>
2016-02-01 16:08:24 -0500

    tes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ import shlex
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 
-sys.path.insert(0,os.path.abspath("..//solar_mod"))
+sys.path.insert(0,os.path.abspath("C://Users//Antoine//Documents//GitHub//Solar_mod//solar_mod"))
 
 # -- General configuration ------------------------------------------------
 
